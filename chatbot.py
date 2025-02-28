eda10327ed0639c9ee1e210ebc0d6212acb7e78e
RongalaPravalika <rongalapravalika2006@gmail.com>
2025-02-28 10:13:32 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -15,7 +15,7 @@ if "messages" not in st.session_state:
 for message in st.session_state.messages:
   with st.chat_message(message["role"]):
     st.markdown(message["content"])
-if prompt :=st.chart_input("Say something..."):
+if prompt :=st.chat_input("Say something..."):
   st.session_state.message.append({"role": "user","content":prompt})
   with st.chat_message("user"):
     st.markdown(prompt)
