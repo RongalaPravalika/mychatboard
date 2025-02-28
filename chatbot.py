4f6c6f426fc820ecc25186b54e72947563771ae9
RongalaPravalika <rongalapravalika2006@gmail.com>
2025-02-28 10:23:31 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -16,11 +16,11 @@ for message in st.session_state.messages:
   with st.chat_message(message["role"]):
     st.markdown(message["content"])
 if prompt :=st.chat_input("Say something..."):
-  st.session_state.message.append({"role": "user","content":prompt})
+  st.session_state.messages.append({"role": "user","content":prompt})
   with st.chat_message("user"):
     st.markdown(prompt)
 
   response=st.session_state.chat.send_message(prompt)
-  st.session_state.message.append({"role": "assistant", "content": response.text})
+  st.session_state.messages.append({"role": "assistant", "content": response.text})
   with st.chat_message("assistant"):
     st.markdown(response.text)
